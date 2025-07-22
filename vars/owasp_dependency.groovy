af6b6f2b4d1297add1fa9d4e722cb5a6b106c304
Huzaifa Hatim <huzaifahatim2000@hotmail.com>
2025-07-22 07:33:30 +0500

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,10 @@
-def call(){
+def call() {
+
+  // Ensure the output folder exists
+  sh 'mkdir -p reports'
+
+  // Run the OWASP Dependency-Check scan with XML output
   dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+
 }
