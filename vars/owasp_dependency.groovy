0a8d553d3f167a87c14478b505af0fd4be33e631
Huzaifa Hatim <huzaifahatim2000@hotmail.com>
2025-07-22 09:44:09 +0500

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,29 +1,5 @@
 def call() {
-    sh '''
-        mkdir -p reports
-        dependency-check.sh \
-            --project Wanderlust \
-            --scan ./ \
-            --format XML \
-            --out reports \
-            --disableAssembly
-    '''
-    
-    // Confirm the file was created
-    sh 'ls -l reports/dependency-check-report.xml || echo "XML report not generated"'
-
-    // Publish XML report
-    dependencyCheckPublisher pattern: 'reports/dependency-check-report.xml'
-}
-
-
-
-
-
-
-/*def call() {
     sh 'mkdir -p reports'
     dependencyCheck additionalArguments: '--scan ./ --format XML --out reports', odcInstallation: 'OWASP'
     dependencyCheckPublisher pattern: 'reports/dependency-check-report.xml'
 }
-*/
